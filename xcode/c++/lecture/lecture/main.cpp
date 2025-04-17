a478575bd388bdf11e166ed679011dfd83a8f24d
ntjjang <ntjjang@gmail.com>
2025-04-17 15:44:02 +0900

    first

---
+++ xcode/c++/lecture/lecture/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lecture
+//
+//  Created by 박종덕 on 4/17/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
